40ba0463b9be60b0e3b872af0c995012d817e983
Rafael Villar Burke <pachi@ietcc.csic.es>
2020-07-27 15:27:19 +0200

    wallcons: usa resistencia intrínseca en lugar de u intrínseca

--- src/bdl/db/wallcons.rs
+++ src/bdl/db/wallcons.rs
@@ -5,7 +5,7 @@
 use failure::Error;
 use std::{collections::HashMap, convert::TryFrom};
 
-use crate::bdl::{extract_f32vec, extract_namesvec, BdlBlock, Boundaries, Material, Tilt};
+use crate::bdl::{extract_f32vec, extract_namesvec, BdlBlock, Boundaries, Material, Tilt, Wall};
 use crate::utils::fround2;
 
 /// Definición de elemento a través de sus capas
@@ -29,8 +29,8 @@ impl WallCons {
         self.thickness.iter().sum()
     }
 
-    /// Transmitancia térmica de una composición de capas [W/m2K]
-    pub fn u_intrinsic(&self, materialsdb: &HashMap<String, Material>) -> Result<f32, Error> {
+    /// Resistencia térmica de una composición de capas [W/m2K]
+    pub fn r_intrinsic(&self, materialsdb: &HashMap<String, Material>) -> Result<f32, Error> {
         let materials = &self
             .material
             .iter()
@@ -56,17 +56,9 @@ impl WallCons {
             })
             // Resistencia térmica total
             .try_fold(0.0_f32, |acc, x| x.map(|res| res + acc))
-            // Transmitancia térmica
-            .and_then(|resvec| {
-                if resvec != 0.0 {
-                    Some(1.0 / resvec)
-                } else {
-                    None
-                }
-            })
             .ok_or_else(|| {
                 format_err!(
-                    "Error al calcular la transmitancia de la composición \"{}\"",
+                    "Error al calcular la resistencia térmica de la composición \"{}\"",
                     self.name
                 )
             })
@@ -82,15 +74,14 @@ impl WallCons {
     ///       las construcciones por defecto
     /// - los elementos adiabáticos se reportan con valor 0.0
     /// - las particiones interiores horizontales
-    pub fn u(
-        &self,
-        bounds: Boundaries,
-        position: Tilt,
-        materialsdb: &HashMap<String, Material>,
-    ) -> f32 {
+    pub fn u(&self, wall: &Wall, materialsdb: &HashMap<String, Material>) -> f32 {
         use Boundaries::*;
         use Tilt::*;
-        let u = self.u_intrinsic(&materialsdb).unwrap_or_default();
+
+        let bounds = wall.bounds;
+        let position = wall.position();
+
+        let r_intrinsic = self.r_intrinsic(&materialsdb).unwrap_or_default();
 
         // Resistencias superficiales [m2·K/W]
         // Revisar según DA-DB-HE/1 tabla 1
@@ -106,19 +97,19 @@ impl WallCons {
                 // TODO: implementar muros enterrados
                 SIDE => Default::default(),
                 // Cubiertas enterradas: el terreno debe estar definido como una capa de tierra con lambda = 2 W/K
-                TOP => 1.0 / (1.0 / u + RSI_ASCENDENTE + RSE),
+                TOP => 1.0 / (r_intrinsic + RSI_ASCENDENTE + RSE),
             },
             // Tomamos valor 0.0. Siempre se podría consultar la resistencia intrínseca
             ADIABATIC => 0.0,
             // HULC no diferencia entre posiciones para elementos interiores
             // TODO: Detectar el caso de contacto con espacios no habitables, con cálculo de b, e implementar
             // TODO: tal vez esto debería recibir el valor b como parámetro
-            INTERIOR => 1.0 / (1.0 / u + 2.0 * RSI_HORIZONTAL),
+            INTERIOR => 1.0 / (r_intrinsic + 2.0 * RSI_HORIZONTAL),
             // Elementos en contacto con el exterior
             EXTERIOR => match position {
-                BOTTOM => 1.0 / (1.0 / u + RSI_DESCENDENTE + RSE),
-                TOP => 1.0 / (1.0 / u + RSI_ASCENDENTE + RSE),
-                SIDE => 1.0 / (1.0 / u + RSI_HORIZONTAL + RSE),
+                BOTTOM => 1.0 / (r_intrinsic + RSI_DESCENDENTE + RSE),
+                TOP => 1.0 / (r_intrinsic + RSI_ASCENDENTE + RSE),
+                SIDE => 1.0 / (r_intrinsic + RSI_HORIZONTAL + RSE),
             },
         };
         fround2(u_noround)
