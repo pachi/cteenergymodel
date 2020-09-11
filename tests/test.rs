d138206bc69c177c429ed35949647003ab2fe84d
Rafael Villar Burke <pachi@ietcc.csic.es>
2020-09-11 19:31:30 +0200

    test: comparación de float con tolerancia

--- tests/test.rs
+++ tests/test.rs
@@ -37,10 +37,10 @@ fn test_polygon() {
         .parse()
         .unwrap();
     let pol: Polygon = Polygon::try_from(polblk).unwrap();
-    assert_eq!(pol.area(), 76.306793);
+    assert_almost_eq!(pol.area(), 76.306793, 0.001);
     assert_eq!(pol.edge_indices("V1").unwrap(), [0, 1]);
     assert_eq!(pol.edge_indices("V6").unwrap(), [5, 0]);
-    assert_eq!(pol.edge_length("V3"), 18.22 - 10.86);
+    assert_almost_eq!(pol.edge_length("V3"), 18.22 - 10.86, 0.001);
 }
 
 #[test]
@@ -58,29 +58,29 @@ fn test_polygon2() {
         .parse()
         .unwrap();
     let pol: Polygon = Polygon::try_from(polblk).unwrap();
-    assert_eq!(pol.area(), 4.5);
-    assert_eq!(pol.perimeter(), 8.2426405);
+    assert_almost_eq!(pol.area(), 4.5, 0.001);
+    assert_almost_eq!(pol.perimeter(), 8.2426405, 0.001);
     assert_eq!(pol.edge_indices("V1").unwrap(), [0, 1]);
     assert_eq!(pol.edge_indices("V6").unwrap(), [5, 0]);
-    assert_eq!(pol.edge_length("V3"), 1.0);
+    assert_almost_eq!(pol.edge_length("V3"), 1.0, 0.001);
     // lado horizontal hacia la derecha
-    assert_eq!(pol.edge_orient("V1", 0.0), 0.0);
+    assert_almost_eq!(pol.edge_orient("V1", 0.0), 0.0, 0.001);
     // lado inclinado 45º hacia la derecha-arriba
-    assert_eq!(pol.edge_orient("V2", 0.0), 45.0);
+    assert_almost_eq!(pol.edge_orient("V2", 0.0), 45.0, 0.001);
     // lado vertical hacia arriba
-    assert_eq!(pol.edge_orient("V3", 0.0), 90.0);
+    assert_almost_eq!(pol.edge_orient("V3", 0.0), 90.0, 0.001);
     // lado horizontal hacia la izquierda
-    assert_eq!(pol.edge_orient("V4", 0.0), 180.0);
+    assert_almost_eq!(pol.edge_orient("V4", 0.0), 180.0, 0.001);
     // lado inclinado 45º hacia la izquierda-abajo
-    assert_eq!(pol.edge_orient("V5", 0.0), 225.0);
+    assert_almost_eq!(pol.edge_orient("V5", 0.0), 225.0, 0.001);
     // lado inclinado 45º hacia la derecha-abajo
-    assert_eq!(pol.edge_orient("V6", 0.0), 315.0);
+    assert_almost_eq!(pol.edge_orient("V6", 0.0), 315.0, 0.001);
     // V1 con norte desviado 45º
-    assert_eq!(pol.edge_orient("V1", 45.0), 315.0);
+    assert_almost_eq!(pol.edge_orient("V1", 45.0), 315.0, 0.001);
     // V5 con norte desviado 45º
-    assert_eq!(pol.edge_orient("V5", 45.0), 180.0);
+    assert_almost_eq!(pol.edge_orient("V5", 45.0), 180.0, 0.001);
     // V2 con norte desviado 45º
-    assert_eq!(pol.edge_orient("V2", 45.0), 0.0);
+    assert_almost_eq!(pol.edge_orient("V2", 45.0), 0.0, 0.001);
 }
 
 #[test]
@@ -93,7 +93,7 @@ fn test_test_spaces_caso_a() {
     for (name, space) in tbl.spaces {
         let spc = bdl.get_space(&name).unwrap();
         let poly = &spc.polygon;
-        assert_eq!(space.area, poly.area())
+        assert_almost_eq!(space.area, poly.area(), 0.001)
     }
 }
 
@@ -130,65 +130,65 @@ fn test_bdl_parse() {
 
     // Espacio
     let s = bdldb.get_space("P02_E01").unwrap();
-    assert_eq!(s.height, 3.0); // Altura
-    assert_eq!(s.space_height(bdldb).unwrap(), 2.62); // Altura libre
-    assert_eq!(s.area(), 150.0); // Área 10m x 15m
-    assert_eq!(s.perimeter(), 50.0); // Perímetro (10 + 15) x 2
+    assert_almost_eq!(s.height, 3.0, 0.001); // Altura
+    assert_almost_eq!(s.space_height(bdldb).unwrap(), 2.62, 0.001); // Altura libre
+    assert_almost_eq!(s.area(), 150.0, 0.001); // Área 10m x 15m
+    assert_almost_eq!(s.perimeter(), 50.0, 0.001); // Perímetro (10 + 15) x 2
 
     // Forjado interior
     let w = bdldb.get_wall("P02_E01_FI001").unwrap();
-    assert_eq!(w.gross_area(bdldb).unwrap(), 49.985004);
-    assert_eq!(w.net_area(bdldb).unwrap(), 49.985004);
+    assert_almost_eq!(w.gross_area(bdldb).unwrap(), 49.985004, 0.001);
+    assert_almost_eq!(w.net_area(bdldb).unwrap(), 49.985004, 0.001);
     assert_eq!(w.space, "P02_E01");
-    assert_eq!(w.tilt, 180.0);
-    assert_eq!(w.azimuth(0.0, bdldb).unwrap(), 0.0); // Horizontal
+    assert_almost_eq!(w.tilt, 180.0, 0.001);
+    assert_almost_eq!(w.azimuth(0.0, bdldb).unwrap(), 0.0, 0.001); // Horizontal
 
     // Solera
     let w = bdldb.get_wall("P01_E01_FTER001").unwrap();
-    assert_eq!(w.gross_area(bdldb).unwrap(), 50.0);
-    assert_eq!(w.net_area(bdldb).unwrap(), 50.0);
+    assert_almost_eq!(w.gross_area(bdldb).unwrap(), 50.0, 0.001);
+    assert_almost_eq!(w.net_area(bdldb).unwrap(), 50.0, 0.001);
     assert_eq!(w.space, "P01_E01");
-    assert_eq!(w.tilt, 180.0);
-    assert_eq!(w.azimuth(0.0, bdldb).unwrap(), 0.0); // Horizontal
+    assert_almost_eq!(w.tilt, 180.0, 0.001);
+    assert_almost_eq!(w.azimuth(0.0, bdldb).unwrap(), 0.0, 0.001); // Horizontal
 
     // Pared exterior
     let w = bdldb.get_wall("P01_E01_PE003").unwrap();
-    assert_eq!(w.azimuth(0.0, bdldb).unwrap(), 0.0); // Norte
+    assert_almost_eq!(w.azimuth(0.0, bdldb).unwrap(), 0.0, 0.001); // Norte
     let w = bdldb.get_wall("P04_E01_ME001").unwrap();
-    assert_eq!(w.azimuth(0.0, bdldb).unwrap(), 0.0); // Norte
-    assert_eq!(w.gross_area(bdldb).unwrap(), 17.5);
+    assert_almost_eq!(w.azimuth(0.0, bdldb).unwrap(), 0.0, 0.001); // Norte
+    assert_almost_eq!(w.gross_area(bdldb).unwrap(), 17.5, 0.001);
 
     // Muro exterior
     let w = bdldb.get_wall("P01_E01_PE001").unwrap();
-    assert_eq!(w.azimuth(0.0, bdldb).unwrap(), 180.0); // Sur
+    assert_almost_eq!(w.azimuth(0.0, bdldb).unwrap(), 180.0, 0.001); // Sur
 
     // Muro exterior
     let w = bdldb.get_wall("P02_E01_PE003").unwrap();
-    assert_eq!(w.azimuth(0.0, bdldb).unwrap(), 90.0); // Este
+    assert_almost_eq!(w.azimuth(0.0, bdldb).unwrap(), 90.0, 0.001); // Este
 
     // Muro interior
     let w = bdldb.get_wall("P02_E01_PE001").unwrap();
-    assert_eq!(w.gross_area(bdldb).unwrap(), 30.0);
-    assert_eq!(w.net_area(bdldb).unwrap(), 28.0);
+    assert_almost_eq!(w.gross_area(bdldb).unwrap(), 30.0, 0.001);
+    assert_almost_eq!(w.net_area(bdldb).unwrap(), 28.0, 0.001);
     assert_eq!(w.space, "P02_E01");
-    assert_eq!(w.tilt, 90.0);
-    assert_eq!(w.azimuth(0.0, bdldb).unwrap(), 270.0); // Oeste
+    assert_almost_eq!(w.tilt, 90.0, 0.001);
+    assert_almost_eq!(w.azimuth(0.0, bdldb).unwrap(), 270.0, 0.001); // Oeste
 
     let v = bdldb.get_window("P02_E01_PE001_V").unwrap();
-    assert_eq!(v.area(), 2.0);
+    assert_almost_eq!(v.area(), 2.0, 0.001);
     assert_eq!(v.wall, "P02_E01_PE001");
-    assert_eq!(v.tilt(bdldb).unwrap(), 90.0);
-    assert_eq!(v.azimuth(0.0, bdldb).unwrap(), 270.0); // Oeste
+    assert_almost_eq!(v.tilt(bdldb).unwrap(), 90.0, 0.001);
+    assert_almost_eq!(v.azimuth(0.0, bdldb).unwrap(), 270.0, 0.001); // Oeste
 
     // Cubiertas
     let w = bdldb.get_wall("P03_E01_CUB001").unwrap();
     assert_almost_eq!(w.gross_area(bdldb).unwrap(), 50.0, 0.005);
-    assert_eq!(w.azimuth(0.0, bdldb).unwrap(), 0.0); // Horizontal
-    assert_eq!(w.tilt, 0.0); // Horizontal
+    assert_almost_eq!(w.azimuth(0.0, bdldb).unwrap(), 0.0, 0.001); // Horizontal
+    assert_almost_eq!(w.tilt, 0.0, 0.001); // Horizontal
     let w = bdldb.get_wall("P04_E01_CUB001").unwrap();
     assert_almost_eq!(w.gross_area(bdldb).unwrap(), 50.99020, 0.005);
-    assert_eq!(w.azimuth(0.0, bdldb).unwrap(), 90.0); // Este
-    assert_eq!(w.tilt, 11.30993);
+    assert_almost_eq!(w.azimuth(0.0, bdldb).unwrap(), 90.0, 0.001); // Este
+    assert_almost_eq!(w.tilt, 11.30993, 0.001);
 
     // // Volumen acondicionado de la envolvente:
     // // - volumen de los espacios acondicionados
@@ -208,7 +208,7 @@ fn test_caso_a() {
     let kygpath = kyg::find_kyg("tests/casoA").unwrap();
     let tblpath = tbl::find_tbl("tests/casoA").unwrap();
     let data = collect_hulc_data(ctehexmlpath, kygpath, tblpath).unwrap();
-    assert_eq!(data.a_ref(), 400.0);
+    assert_almost_eq!(data.a_ref(), 400.0, 0.001);
     assert_eq!(data.meta.climate, ClimateZone::D3);
     assert_eq!(data.windows.len(), 10);
     assert_eq!(data.walls.len(), 35); // 19 en ET
@@ -231,42 +231,42 @@ fn test_caso_a() {
     );
     // Suelo al exterior (aire), HULC=0.34
     let wall = data.walls.get("P02_E01_ME001").unwrap();
-    assert_eq!(fround2(data.u_for_wall(&wall)), 0.33);
+    assert_almost_eq!(fround2(data.u_for_wall(&wall)), 0.33, 0.001);
     // Fachada exterior, HULC=0.30
     let wall = data.walls.get("P01_E01_ME001").unwrap();
-    assert_eq!(fround2(data.u_for_wall(&wall)), 0.30);
+    assert_almost_eq!(fround2(data.u_for_wall(&wall)), 0.30, 0.001);
     // Cubierta exterior, HULC=0.34
     let wall = data.walls.get("P03_E01_FE004").unwrap();
-    assert_eq!(fround2(data.u_for_wall(&wall)), 0.34);
+    assert_almost_eq!(fround2(data.u_for_wall(&wall)), 0.34, 0.001);
     // Muro de sótano (z=0), HULC=0.0 (por no habitable)
     let wall = data.walls.get("P01_E02_TER001").unwrap();
-    assert_eq!(fround2(data.u_for_wall(&wall)), 0.59);
+    assert_almost_eq!(fround2(data.u_for_wall(&wall)), 0.59, 0.001);
     // Solera (z=0), HULC=0.47
     let wall = data.walls.get("P01_E01_FTER001").unwrap();
-    assert_eq!(fround2(data.u_for_wall(&wall)), 0.35);
+    assert_almost_eq!(fround2(data.u_for_wall(&wall)), 0.35, 0.001);
     // Forjado interior, HULC=1.37
     let wall = data.walls.get("P03_E01_FI003").unwrap();
-    assert_eq!(fround2(data.u_for_wall(&wall)), 1.37);
+    assert_almost_eq!(fround2(data.u_for_wall(&wall)), 1.37, 0.001);
     // Partición interior vertical con espacio no habitable, HULC=0.81
     let wall = data.walls.get("P01_E01_Med001").unwrap();
-    assert_eq!(fround2(data.u_for_wall(&wall)), 0.82);
+    assert_almost_eq!(fround2(data.u_for_wall(&wall)), 0.82, 0.001);
     // Partición interior horizontal (suelo) con espacio no habitable y enterrado, HULC=0.65
     let wall = data.walls.get("P02_E01_FI002").unwrap();
-    assert_eq!(fround2(data.u_for_wall(&wall)), 0.54);
+    assert_almost_eq!(fround2(data.u_for_wall(&wall)), 0.54, 0.001);
     // Partición interior horizontal (techo) con espacio no habitable/acondicionado, HULC=0.77
     let wall = data.walls.get("P03_E01_FI001").unwrap();
-    assert_eq!(fround2(data.u_for_wall(&wall)), 0.46);
+    assert_almost_eq!(fround2(data.u_for_wall(&wall)), 0.46, 0.001);
     // Partición interior vertical con espacio no habitable/acondicionado, HULC=0.68
     let wall = data.walls.get("P04_E01_Med001").unwrap();
-    assert_eq!(fround2(data.u_for_wall(&wall)), 0.66);
+    assert_almost_eq!(fround2(data.u_for_wall(&wall)), 0.66, 0.001);
 
     // Cálculo de K, n50, C_o
-    assert_eq!(fround2(data.K_he2019()), 0.51);
-    assert_eq!(fround2(data.n50_he2019()), 4.60);
-    assert_eq!(fround2(data.n50()), 5.32);
-    assert_eq!(fround2(data.n50_he2019()), 4.60);
-    assert_eq!(fround2(data.C_o_he2019()), 16.00);
-    assert_eq!(fround2(data.C_o()), 18.86);
+    assert_almost_eq!(fround2(data.K_he2019()), 0.51, 0.001);
+    assert_almost_eq!(fround2(data.n50_he2019()), 4.60, 0.001);
+    assert_almost_eq!(fround2(data.n50()), 5.32, 0.001);
+    assert_almost_eq!(fround2(data.n50_he2019()), 4.60, 0.001);
+    assert_almost_eq!(fround2(data.C_o_he2019()), 16.00, 0.001);
+    assert_almost_eq!(fround2(data.C_o()), 18.86, 0.001);
 }
 
 #[test]
@@ -275,7 +275,7 @@ fn test_caso_c() {
     let kygpath = kyg::find_kyg("tests/casoC").unwrap();
     let tblpath = tbl::find_tbl("tests/casoC").unwrap();
     let data = collect_hulc_data(ctehexmlpath, kygpath, tblpath).unwrap();
-    assert_eq!(data.a_ref(), 400.0);
+    assert_almost_eq!(data.a_ref(), 400.0, 0.001);
     assert_eq!(data.meta.climate, ClimateZone::D3);
     assert_eq!(data.windows.len(), 9);
     assert_eq!(data.walls.len(), 33); // 27 en ET
@@ -289,7 +289,7 @@ fn parse_test_data() {
     let kygpath = kyg::find_kyg("tests/data").unwrap();
     let tblpath = tbl::find_tbl("tests/data").unwrap();
     let data = collect_hulc_data(ctehexmlpath, kygpath, tblpath).unwrap();
-    assert_eq!(data.a_ref(), 1673.92);
+    assert_almost_eq!(data.a_ref(), 1673.92, 0.001);
     assert_eq!(data.meta.climate, ClimateZone::D3);
     assert_eq!(data.windows.len(), 92);
     assert_eq!(data.walls.len(), 127); // 68 en ET
@@ -309,7 +309,7 @@ fn parse_test_data2() {
     let tblpath = tbl::find_tbl("tests/ejemplopmt_HuecosOK").unwrap();
     // Las versiones más nuevas usan la coma en KyGananciasSolares.txt como separador decimal
     let data = collect_hulc_data(ctehexmlpath, kygpath, tblpath).unwrap();
-    assert_eq!(data.a_ref(), 1063.03);
+    assert_almost_eq!(data.a_ref(), 1063.03, 0.001);
     assert_eq!(data.meta.climate, ClimateZone::B3);
     assert_eq!(data.windows.len(), 29);
     assert_eq!(data.walls.len(), 95); // 60 en ET
