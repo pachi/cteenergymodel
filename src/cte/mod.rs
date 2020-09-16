a39376d0c058f1335f9bdd638541586813ef3057
Rafael Villar Burke <pachi@ietcc.csic.es>
2020-09-16 20:26:53 +0200

    model: reordena propiedades de componentes

--- src/cte/mod.rs
+++ src/cte/mod.rs
@@ -98,43 +98,38 @@ impl Default for Meta {
 
 // Elementos -----------------------------------------------
 
-/// Datos adicionales para comprobación de muros
-#[derive(Debug, Clone, Deserialize, Serialize)]
-pub struct ExtraData {
-    // Nombre del muro
+/// Espacio
+/// XXX: en teoría se podrían asignar los espacios a zonas térmicas, aunque simplificadamente
+/// XXX: consideraremos que cada espacio se corresponde con una ZT.
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Space {
+    /// Nombre del espacio
     pub name: String,
-    // Condiciones de contorno del muro
-    pub bounds: BoundaryType,
-    // Tipo de espacio
-    pub spacetype: SpaceType,
-    // Espacio adyacente
-    pub nextspace: Option<String>,
-    // Tipo de espacio adyacente
-    pub nextspacetype: Option<SpaceType>,
-    // Inclinación del muro
-    pub tilt: Tilt,
-    // Construcción
-    pub cons: String,
-    // U por defecto u obtenida de archivo KyGananciasSolares.txt
-    pub u: f32,
-    // U calculada con UNE-EN ISO 13789
-    pub computed_u: f32,
-}
-
-/// Hueco
-#[derive(Debug, Clone, Default, Serialize, Deserialize)]
-pub struct Window {
-    /// Nombre del hueco
-    pub name: String,
-    /// Construcción del hueco
-    pub cons: String,
-    /// Muro al que pertenece el hueco
-    pub wall: String,
-    /// Superficie del hueco (m2)
-    #[serde(rename = "A")]
+    /// Superficie útil del espacio (m2)
     pub area: f32,
-    /// Factor de obstáculos remotos
-    pub fshobst: f32,
+    /// Multiplicador del espacio
+    pub multiplier: f32,
+    /// Tipo de espacio:
+    /// - CONDITIONED: acondicionado,
+    /// - UNCONDITIONED: no acondicionado
+    /// - UNINHABITED: no habitable
+    #[serde(rename = "type")]
+    pub space_type: SpaceType,
+    /// Pertenencia al interior de la envolvente térmica
+    pub inside_tenv: bool,
+    /// Altura libre (suelo a techo) del espacio (m)
+    /// No incluye el volumen de forjados o cubiertas.
+    pub height_net: f32,
+    /// Altura bruta (suelo a suelo) del espacio (m)
+    pub height_gross: f32,
+    /// Ventilación, en ren/h
+    pub n_v: Option<f32>,
+    /// Cota del espacio respecto al suelo (m)
+    pub z: f32,
+    /// Perímetro expuesto del espacio (suelos) (m)
+    /// Incluye la parte del perímetro que separa el espacio del exterior
+    /// y excluye que lo separa de otros espacios acondicionados.
+    pub exposed_perimeter: Option<f32>,
 }
 
 /// Elemento opaco (muro, cubierta, suelo, partición)
@@ -142,21 +137,21 @@ pub struct Window {
 pub struct Wall {
     /// Nombre del elemento opaco
     pub name: String,
-    /// Construcción del opaco
-    pub cons: String,
-    /// Espacio al que pertenece el elemento opaco
-    pub space: String,
-    /// Espacio adyacente con el que comunica el elemento opaco cuando es interior
-    pub nextto: Option<String>,
+    /// Superficie neta (sin huecos) del elemento opaco (m2)
+    #[serde(rename = "A")]
+    pub area: f32,
     /// Condiciones de contorno del cerramiento:
     /// - GROUND: cerramientos en contacto con el terreno
     /// - EXTERIOR: cerramientos en contacto con el aire exterior
     /// - INTERIOR: cerramientos en contacto con el aire de otros espacios
     /// - ADIABATIC: cerramientos sin transmisión de calor
     pub bounds: BoundaryType,
-    /// Superficie neta (sin huecos) del elemento opaco (m2)
-    #[serde(rename = "A")]
-    pub area: f32,
+    /// Construcción del opaco
+    pub cons: String,
+    /// Espacio al que pertenece el elemento opaco
+    pub space: String,
+    /// Espacio adyacente con el que comunica el elemento opaco cuando es interior
+    pub nextto: Option<String>,
     /// Orientación (gamma) [-180,+180] (S=0, E=+90, W=-90)
     /// Medido como azimuth geográfico de la proyección horizontal de la normal a la superficie
     /// Coincide con el criterio de la UNE-EN ISO 52016-1
@@ -165,6 +160,22 @@ pub struct Wall {
     /// Inclinación (beta) [0, 180]
     /// Medido respecto a la horizontal y normal hacia arriba (0 -> suelo, 180 -> techo)
     pub tilt: f32,
+}
+
+/// Hueco
+#[derive(Debug, Clone, Default, Serialize, Deserialize)]
+pub struct Window {
+    /// Nombre del hueco
+    pub name: String,
+    /// Superficie del hueco (m2)
+    #[serde(rename = "A")]
+    pub area: f32,
+    /// Construcción del hueco
+    pub cons: String,
+    /// Muro al que pertenece el hueco
+    pub wall: String,
+    /// Factor de obstáculos remotos
+    pub fshobst: f32,
 }
 
 /// Puente térmico
@@ -177,40 +188,6 @@ pub struct ThermalBridge {
     pub l: f32,
     /// Transmitancia térmica lineal del puente térmico (W/mK)
     pub psi: f32,
-}
-
-/// Espacio
-/// XXX: en teoría se podrían asignar los espacios a zonas térmicas, aunque simplificadamente
-/// XXX: consideraremos que cada espacio se corresponde con una ZT.
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct Space {
-    /// Nombre del espacio
-    pub name: String,
-    /// Superficie útil del espacio (m2)
-    pub area: f32,
-    /// Cota del espacio respecto al suelo (m)
-    pub z: f32,
-    /// Perímetro expuesto del espacio (suelos) (m)
-    /// Incluye la parte del perímetro que separa el espacio del exterior
-    /// y excluye que lo separa de otros espacios acondicionados.
-    pub exposed_perimeter: Option<f32>,
-    /// Altura libre (suelo a techo) del espacio (m)
-    /// No incluye el volumen de forjados o cubiertas.
-    pub height_net: f32,
-    /// Altura bruta (suelo a suelo) del espacio (m)
-    pub height_gross: f32,
-    /// Pertenencia al interior de la envolvente térmica
-    pub inside_tenv: bool,
-    /// Multiplicador del espacio
-    pub multiplier: f32,
-    /// Tipo de espacio:
-    /// - CONDITIONED: acondicionado,
-    /// - UNCONDITIONED: no acondicionado
-    /// - UNINHABITED: no habitable
-    #[serde(rename = "type")]
-    pub space_type: SpaceType,
-    /// Ventilación, en ren/h
-    pub n_v: Option<f32>,
 }
 
 /// Definición de construcción de elemento opaco
@@ -247,4 +224,27 @@ pub struct WindowCons {
     /// Permeabilidad al aire a 100 Pa [m3/hm2]
     #[serde(rename = "C_100")]
     pub infcoeff_100: f32,
+}
+
+/// Datos adicionales para comprobación de muros
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct ExtraData {
+    // Nombre del muro
+    pub name: String,
+    // Condiciones de contorno del muro
+    pub bounds: BoundaryType,
+    // Tipo de espacio
+    pub spacetype: SpaceType,
+    // Espacio adyacente
+    pub nextspace: Option<String>,
+    // Tipo de espacio adyacente
+    pub nextspacetype: Option<SpaceType>,
+    // Inclinación del muro
+    pub tilt: Tilt,
+    // Construcción
+    pub cons: String,
+    // U por defecto u obtenida de archivo KyGananciasSolares.txt
+    pub u: f32,
+    // U calculada con UNE-EN ISO 13789
+    pub computed_u: f32,
 }
