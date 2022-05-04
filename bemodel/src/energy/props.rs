760af381fbf7aa01a36395ccf62150c3430ecdcc
Rafael Villar Burke <pachi@ietcc.csic.es>
2022-05-04 19:41:29 +0200

    bemodel: añade espesor de construcción de opacos

--- bemodel/src/energy/props.rs
+++ bemodel/src/energy/props.rs
@@ -39,6 +39,7 @@ impl From<&Model> for EnergyProps {
         let mut wallcons: BTreeMap<Uuid, WallConsProps> = BTreeMap::new();
         for wc in &model.cons.wallcons {
             let wcp = WallConsProps {
+                thickness: wc.thickness(),
                 r_intrinsic: wc.r_intrinsic(&model.cons).ok(),
             };
             wallcons.insert(wc.id, wcp);
@@ -381,6 +382,8 @@ pub struct TbProps {
 /// Propiedades de construcciones de opacos
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct WallConsProps {
+    /// Espesor total de la construcción, [m]
+    pub thickness: f32,
     // R intrínseca de construcción, [m²K/W]
     pub r_intrinsic: Option<f32>,
 }
